c6fb8880e06ba52369e76fe27ea8da2c2aee0d0e
Alexey <azarenkov20141@icloud.com>
2025-01-24 08:19:38 +0500

    Created login Service

--- src/main/java/org/example/services/StudentService.java
+++ src/main/java/org/example/services/StudentService.java
@@ -2,6 +2,7 @@ package org.example.services;
 
 import org.example.exceptions.StudentNotFoundException;
 import org.example.models.Student;
+import org.example.models.Teacher;
 import org.example.repositories.StudentRepositoryInterface;
 import org.example.services.interfaces.StudentServiceInterface;
 import org.springframework.data.domain.Sort;
@@ -11,25 +12,5 @@ import java.util.List;
 
 @Service
 public class StudentService implements StudentServiceInterface {
-    private final StudentRepositoryInterface repo;
 
-    public StudentService(StudentRepositoryInterface repo) {
-        this.repo = repo;
-    }
-
-    @Override
-    public List<Student> getAll() {
-        return repo.findAll(Sort.by("name"));
-    }
-
-    @Override
-    public Student getByEmail(String email) {
-        return repo.findByEmail(email);
-
-    }
-
-    @Override
-    public Student create(Student student) {
-        return repo.save(student);
-    }
 }
